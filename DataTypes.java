345a71da4bcd51823161c5b0a96810c3e7fd2aef
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-21 17:02:17 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class DataTypes
+{
+	public static void main(String[] args)
+	{
+	    int a=10;
+	    float b=12.53f;
+	    double dd=19098847;
+	    char c='A';
+	    boolean bt=true;
+	    System.out.println("integer : "+a);
+	    System.out.println("float : "+b);
+	    System.out.println("double : "+dd);
+	    System.out.println("character : "+c);
+	    System.out.println("boolean : "+bt);
+	    
+	}
+}
