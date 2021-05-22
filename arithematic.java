f2556af1617575ce7bdd2fcabcc4ca26b2aac79f
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-22 15:13:50 +0530

    Create arithematic.java

---
+++ arithematic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+class arithematic
+{
+    public static void main(String args[])
+    {
+        int a=20,b=10,c,d,e,f;
+        c=a+b;
+        d=a-b;
+        e=a*b;
+        f=a/b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
