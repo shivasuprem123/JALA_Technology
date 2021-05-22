fb2561ab2c8216baca61bddc41351e2771b2ae84
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-22 16:05:42 +0530

    Create compare6.java

---
+++ compare6.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+class compare6
+{
+    public static void main(String args[])
+    {
+        Scanner c=new Scanner(System.in);
+        int a=c.nextInt();
+        int b=c.nextInt();
+        int d=c.nextInt();
+        if (a<=b)
+        System.out.println(a);
+        else if(d>a)
+        System.out.println(d);
+        if (b>=d)
+        System.out.println(b);
+        else if (d<b)
+        System.out.println(d);
+        
+        
+        
+    }
+}
