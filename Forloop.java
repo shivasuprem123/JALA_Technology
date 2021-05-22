c657f10700d1baae7265f6578041a95a09404f77
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-22 15:27:59 +0530

    Create Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class Main
+{
+  public static void main (String args[])
+  {
+    String str = "Bright IT Career";
+    for(int i=1;i<=10;i++)
+    {
+        System.out.println(i+"-"+str);
+    }
+  }
+}
+    
