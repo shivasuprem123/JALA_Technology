f0eb68d6e647f4076902a6122076c7a8fcff1b73
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-22 15:24:57 +0530

    Update incdec.java

--- incdec.java
+++ incdec.java
@@ -5,11 +5,21 @@ class incdec
     public static void main(String args[])
     {
         int a=5,b=6;
-        a=a+b;
         a++;
-        b=b+a;
-        b--;
         System.out.println(a);
+        ++a;
+        System.out.println(a);
+        --a;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+        b++;
+        System.out.println(b);
+        ++b;
+         System.out.println(b);
+        b--;
+         System.out.println(b);
+        --b;
         System.out.println(b);
     }
 }
