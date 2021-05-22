6a55e13c52c5be6d8922ee3f701aedb83019f378
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-22 15:19:55 +0530

    Create incdec.java

---
+++ incdec.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class incdec
+{
+    public static void main(String args[])
+    {
+        int a=5,b=6;
+        a=a+b;
+        a++;
+        b=b+a;
+        b--;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
