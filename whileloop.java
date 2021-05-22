b5bf63a55089ee04fc566c2d3c9178c612790140
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-22 15:29:20 +0530

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+class Main
+{
+  public static void main (String args[])
+  {
+   int i=0;
+    while(i!=20)
+    {
+        i++;
+        System.out.println(i);
+        
+    }
+  }
+}
+    
