550a9348a0beb89bf45edbacc7ce96127b71979e
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-22 15:23:56 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class compare
+{
+    public static void main(String args[])
+    {
+        int a=5,b=6;
+        if(a==b)
+        System.out.println("true");
+        else if(a!=b)
+        System.out.println("false");
+    }
+}
