373b53936ca42d9793f1840c02c7299fb65c1511
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-22 15:29:16 +0530

    Create equal.java

---
+++ equal.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class equal
+{
+    public static void main(String args[])
+    {
+        Scanner c=new Scanner(System.in);
+        int a=c.nextInt();
+        int b=c.nextInt();
+        if(a==b)
+        System.out.println("equal");
+        else if(a!=b)
+        System.out.println("not equal");
+    }
+}
