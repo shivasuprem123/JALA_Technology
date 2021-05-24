e668e29f20a248616bf7c4dce10c81a87c742c16
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-24 16:02:17 +0530

    Create Static2.java

---
+++ Static2.java
@@ -0,0 +1,15 @@
+
+class Employee
+{
+    int id=101;
+    String name="shiva";
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        Employee e=new Employee();
+        System.out.println(e.id+" "+e.name);
+    }
+}
+        
