0c3a93c3ffec66e1d554900b3903a641d9eff56d
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-24 13:41:48 +0530

    Create loop6.java

---
+++ loop6.java
@@ -0,0 +1,13 @@
+public class loop6
+{
+	public static void main(String[] args) 
+	{
+	    int i=10;
+        while(i<=100)
+        {
+            if(i%2==0)
+            System.out.print(" "+i);
+            i++;
+        }
+	}
+}
