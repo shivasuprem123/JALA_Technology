8828cd09c434111e08558d591ef7d438f3d86cbe
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-24 14:23:56 +0530

    Create loop7.java

---
+++ loop7.java
@@ -0,0 +1,13 @@
+public class loop7
+{
+	public static void main(String[] args) 
+	{
+	    int i=1;
+	    do
+	        {
+	        System.out.print(" "+i);
+	        i++;
+	        }while(i<=10);
+	    
+	}
+}
