8483f8cc46687a6a42323ccd52c3718bdb3dab3f
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-24 13:15:24 +0530

    Create Loops4.java

---
+++ Loops4.java
@@ -0,0 +1,13 @@
+public class Loops4
+{
+	public static void main(String[] args) {
+	    int i=0,n=10;
+	    for(i=0;i<=n;i++)
+	    {
+	       if(i%2==0)
+	       System.out.println(i+" is even");
+	       else 
+	       System.out.println(i+" is odd");
+	    }
+	}
+}
