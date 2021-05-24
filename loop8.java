6404c1549454d8312b7cbc2ebe7f72e5a3d2c357
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-24 15:16:45 +0530

    Update loop8.java

--- loop8.java
+++ loop8.java
@@ -3,17 +3,26 @@ public class loop8
 	public static void main(String[] args) 
 	{
 	int c=0,a,temp;  
-    int n=154;  
+    int n=548834;
+    int length = String.valueOf(n).length();
     temp=n;  
+    int p=1;
     while(n>0)  
     {  
+        p=1;
     a=n%10;  
-    n=n/10;  
-    c=c+(a*a*a);  
-    }  
+    n=n/10;
+     for(int i=0;i<length;i++)
+     {
+          p=p*a;
+     }
+     c=c+p;
+    }
+
     if(temp==c)  
     System.out.println("armstrong number");   
     else  
         System.out.println("Not armstrong number");   
    }  
 }  
+
