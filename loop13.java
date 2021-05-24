28c8351826d5369179e566b4bbee16714397054d
shiva <51490348+shivasuprem123@users.noreply.github.com>
2021-05-24 16:07:45 +0530

    Create loop13.java

---
+++ loop13.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class loop13
+{
+	public static void main(String[] args) 
+	{
+	   int a=10,b=20,c=30;
+	   if(a>b)
+	   {
+	       if (a>c)
+	       System.out.println(a+" is greater");
+	   }
+	   else if(b>c)
+	   {
+	       System.out.println(b+" is greater");
+	   }
+	   else
+	   {
+	       System.out.println(c+" is greater");   
+	   }
+	}
+}
